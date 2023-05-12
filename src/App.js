e3141705958254cd078a1f9cca905f972a6715df
gharkness <garrett1440@gmail.com>
2023-05-12 12:17:53 -0500

    Setup basic starter skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <div className="h-100 w-full flex items-center justify-center font-sans flex-col pb-4">
+            <div className="pb-4">
+                <h1 className="text-7xl font-bold">
+                    Todo List
+                </h1>
+            </div>
+            <div className="lg:w-1/2 flex flex-row">
+                <input placeholder="Add a todo!" className="pr-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-3/4 p-2.5 " />
+                <div className="pl-3">
+                    <button type="button"
+                            className="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5" >Add Todo
+                    </button>
+                </div>
+            </div>
+            <div className="pt-4">
+              <ul className="list-disc px-3">
+                  <li>Todo</li>
+                  <li>Todo</li>
+                  <li>Todo</li>
+              </ul>
+            </div>
+          </div>
+      </div>
   );
 }
 
